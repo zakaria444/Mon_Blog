08e8a5f98bd9d3e417ab6a5b545a6b5d6f567fbb
Zakaria <elismailizakariae@gmail.com>
2021-05-04 11:12:30 +0000

    addinge desinge par admine page

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root'); #username phpmyadmin
+  define('DB_PASS', ''); # password phpmyadmin
+  define('DB_NAME', 'blog'); #database name
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/Mon_blog');
+  // Site Name
+  define('SITENAME', 'My Blog ');
+  // App Version
+  define('APPVERSION', '1.0.0');
